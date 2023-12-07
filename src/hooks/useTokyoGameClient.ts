10c4ecbaa8de0fb02e7eacc3e46f8ce77c006a9f
tuanddd <vincenttuandao@gmail.com>
2023-12-07 13:05:24 +0700

    chore: change server host

--- src/hooks/useTokyoGameClient.ts
+++ src/hooks/useTokyoGameClient.ts
@@ -1,51 +1,48 @@
-import { useEffect, useState } from "react"
-import { v4 as uuidv4 } from 'uuid';
-import {
-    TokyoGameClient as TokyoClient, 
-    Gamepad, 
-} from "tokyoclient-ts"
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { TokyoGameClient as TokyoClient, Gamepad } from "tokyoclient-ts";
 
 interface UseTokyoGameClientParams {
-    onConnectSucceed?: () => void
-    eventHandler?: () => void
-    allowConnect?: boolean
-    userName: string
+  onConnectSucceed?: () => void;
+  eventHandler?: () => void;
+  allowConnect?: boolean;
+  userName: string;
 }
 
 export const useTokyoGameClient = ({
-    userName,
-    allowConnect=true
+  userName,
+  allowConnect = true,
 }: UseTokyoGameClientParams) => {
-    const [ gamepad, setGamepad ] = useState<Gamepad>()
-    const [ isLoading, setIsLoading ] = useState(true)
+  const [gamepad, setGamepad] = useState<Gamepad>();
+  const [isLoading, setIsLoading] = useState(true);
 
-    const createGameClient = () => {
-        if (!allowConnect) {
-            setGamepad(undefined)
-            return
-        }
-        setIsLoading(true)
-        const client = new TokyoClient({
-            serverHost: "combat.sege.dev",
-            apiKey: uuidv4(),
-            useHttps: true,
-            userName: userName + "_" + Math.round(Math.random()*1000)
-        })
-        client.setOnOpenFn(() => setIsLoading(false))
-        setGamepad(client.GamePad())
-        console.log("created client")
-        return () => {
-            if (allowConnect) {
-                console.log('closed')
-                client.close()
-            }
-        }
+  const createGameClient = () => {
+    if (!allowConnect) {
+      setGamepad(undefined);
+      return;
     }
-    useEffect(createGameClient, [allowConnect, userName])
+    setIsLoading(true);
+    const client = new TokyoClient({
+      serverHost: "spacebot-rs.fly.dev",
+      apiKey: uuidv4(),
+      useHttps: true,
+      userName: userName + "_" + Math.round(Math.random() * 1000),
+    });
+    client.setOnOpenFn(() => setIsLoading(false));
+    setGamepad(client.GamePad());
+    console.log("created client");
+    return () => {
+      if (allowConnect) {
+        console.log("closed");
+        client.close();
+      }
+    };
+  };
+  useEffect(createGameClient, [allowConnect, userName]);
 
-    return {
-        // Uncaught DOMException: Failed to execute 'send' on 'WebSocket': Still in CONNECTING state.
-        controller: isLoading ? undefined: gamepad,
-        isLoading,
-    }
-}
+  return {
+    // Uncaught DOMException: Failed to execute 'send' on 'WebSocket': Still in CONNECTING state.
+    controller: isLoading ? undefined : gamepad,
+    isLoading,
+  };
+};
